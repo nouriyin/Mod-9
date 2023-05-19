1a3d92f6c56e93aaa55146a084906104e5d0b5cb
nouriyin <courtneyy.noreen@gmail.com>
2023-05-19 01:33:06 -0400

    added the rest of my code, and made sure everything works

--- generated-README.md
+++ generated-README.md
@@ -1,1 +1,43 @@
 # README
+
+## Description
+
+This application is a readme file generator that uses Node.js to fill in the information that someone answers the questions to. I made this for a module assignment for coding bootcamp.
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+Go to the repo on my github account named "Mod-9", copy the code and add to your local device.
+
+## Usage
+
+After it is copied to your local device, you can fill out these questions in your terminal to generate a professional README.
+
+## Contributions
+
+Courtney Clarke and tutors
+
+## License
+
+IBM
+
+## Questions
+
+nouriyin,
+courtneyy.noreen@gmail.com,
+
+## Tests
+
+Testing can be done through copying the code to your local device and open in Node. In order to see if it works, answer the questions and a readme file will appear.
+
+## Screencast
+
+[https://drive.google.com/file/d/1B8xAPn40Inf3h0IISMLlPHpi0nb0MgJr/view]
--- index.js
+++ index.js
@@ -1,63 +1,82 @@
 // TODO: Include packages needed for this application
 
-const inquirer= require ("inquirer");
-const fs= require ("fs")
-const generateMarkdown=require("./utils/generatemarkdown")
+const inquirer = require("inquirer");
+const fs = require("fs")
+const generateMarkdown = require("./utils/generatemarkdown")
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
-        name: "data.title",
+        name: "title",
         message: "What is the title for your project?",
         validate: (input) => {
             return input ? true : 'Please enter a title for your project';
-    },
+        },
 
     },
-    { 
+    {
         type: "input",
         name: "githubUsername",
         message: "What is your GitHub Username?",
         validate: (input) => {
             return input ? true : 'Please enter your GitHub Username.';
-         },
+        },
     },
-    { 
+    {
+        type: "Installation",
+        name: "installation",
+        message: "What are the installation instructions?",
+        validate: (input) => {
+            return input ? true : 'Please enter installation instructions';
+        }
+
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How would someone use this application?",
+        validate: (input) => {
+            return input ? true : 'Please enter usage information';
+        }
+    },
+    {
+        type: "input",
+        name: "contributions",
+        message: "Please list all contributors",
+        validate: (input) => {
+            return input ? true : 'List all contributors'
+        }
+    },
+    {
         type: "input",
         name: "email",
         message: "What is your email address?",
         validate: (input) => {
             return input ? true : 'Please enter your email address.';
-         },
-    },{ 
+        },
+    }, {
         type: "input",
         name: "description",
         message: "Describe your application",
         validate: (input) => {
             return input ? true : 'Please enter a description of your project.';
-         },
-    },{ 
+        },
+    }, {
         type: "input",
-        name: "explination",
-        message: "Why did you create this project?",
+        name: "test",
+        message: "What are the testing instructions?",
         validate: (input) => {
-            return input ? true : 'Please enter your GitHub Username.';
-         },
-    },{ 
-        type: "input",
-        name: "framework",
-        message: "What framwork did you use?",
-        validate: (input) => {
-            return input ? true : 'Please enter the framework.';
-         },
-    },{ 
+            return input ? true : 'Please enter testing instructions';
+        },
+    },
+    {
         type: "list",
         name: "license",
         message: "Please choose a license",
-        choices: ['Apache', 'IBM', 'MIT', 'ISC','Mozilla', 'None'],
+        choices: ['Apache', 'IBM', 'MIT', 'ISC', 'Mozilla', 'None'],
         validate: (input) => {
             return input ? true : 'Please choose license';
-         },
+        },
     },
 ];
 
@@ -70,12 +89,12 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
-    return inquirer.prompt(questions)
-    .then( (answers) => {
+function init() { }
+return inquirer.prompt(questions)
+    .then((answers) => {
         const markdown = generateMarkdown(answers);
         writeToFile('generated-README.md', markdown);
-        console.log ("README.md file has been created!")
+        console.log("README.md file has been created!")
     })
     .catch((error) => {
         console.log(error)
